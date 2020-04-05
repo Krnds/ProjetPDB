081b6eccb1d5bab0864ed33ec159223893dfce72
Karine Dias <harlequinuniform@gmail.com>
2020-04-05 21:25:39 +0200

    Change instance vars to private and rewite getters for file paths

--- src/main/fr/karinedias/query/FetchStructure.java
+++ src/main/fr/karinedias/query/FetchStructure.java
@@ -17,32 +17,35 @@ public class FetchStructure {
 	 * ETAPE-1 : Isoler toutes les structures de la PDB correspondant à des complexes moléculaires
 	 * Cette classe permet de télécharger automatiquement un fichier de structure d'une protéine de la PDB à partir de son identifiant (ID)
 	 * TODO:
-	 * - ne fonctionne pas si la structure est invalide
+	 * - pour l'appel de la méthode getRandomID dans le constructeur, utiliser plutôt le Singleton DP ?
 	 * - dans ces cas, il faut gérer les exceptions et recommencer les processus
 	 * - quand j'ai la liste de tous les complexes moléculaires, il faut que je trouve un moyen d'en choisir un (random ?)
-	 * et d'ensuite choisir 2 RESIDUS de ce complexe choisi pour calculer les distances...
+	 * - pouvoir choisir 2 RESIDUS de ce complexe choisi pour calculer les distances...
 	 */
 
 	private static final String URL = "https://files.rcsb.org/view/";
 	private static String STRUCTUREID;
 
-	public FetchStructure() throws IOException {
-		STRUCTUREID = Query.getRandomID(); //use singleton design pattern ?
+	public FetchStructure() {
+		try {
+		STRUCTUREID = Query.getRandomID(); //use singleton design pattern to only create a single object Query once and for all ?
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
 	}
 
-
-	public void getPDBFile(String id) {
+	public void getPDBFile() {
 
 		URL url;
 
 		try {
 
-			String finalUrl = URL + id + ".cif"; //or use getUrl ??
+			String finalUrl = URL + STRUCTUREID + ".cif"; //or use getUrl ??
 			System.out.println(finalUrl);
 			url = new java.net.URL(finalUrl);
 
 			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-			FileWriter pdbPath = new FileWriter(getPathOfPDBFiles() + id.toLowerCase() + ".cif");
+			FileWriter pdbPath = new FileWriter(getPathOfPDBFiles() + STRUCTUREID + ".cif");
 			BufferedWriter writer = new BufferedWriter(pdbPath);
 
 			String line;
@@ -66,19 +69,6 @@ public class FetchStructure {
 
 	}
 
-	// FOR TESTING PURPOSES
-	public static void main(String[] args) throws IOException {
-
-		
-		FetchStructure test = new FetchStructure();
-		//ask for id
-		String id = FetchStructure.STRUCTUREID;
-		test.getPDBFile(id);
-		String pathOfCif = FetchStructure.getPathStructure();
-		System.out.println(pathOfCif);
-		
-	}
-
 	
 	/*
 	 * Method for finding the path of the doc directory
@@ -89,7 +79,7 @@ public class FetchStructure {
 		return System.getProperty("user.dir") + File.separator + "doc" + File.separator;
 	}
 	
-	public static String getPathStructure() {
+	public String getPathStructure() {
 		
 		return System.getProperty("user.dir") + File.separator + "doc" + File.separator + getSTRUCTUREID() + ".cif";
 	}
