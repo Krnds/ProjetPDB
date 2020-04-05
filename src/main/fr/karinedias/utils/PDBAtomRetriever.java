515818eb55c4940e0562fd37fc78b6c7c15781b3
Karine Dias <harlequinuniform@gmail.com>
2020-04-05 21:34:43 +0200

    Add comparison of execution time between 2 ways of getting atoms execution time

--- src/main/fr/karinedias/utils/PDBAtomRetriever.java
+++ src/main/fr/karinedias/utils/PDBAtomRetriever.java
@@ -4,12 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.concurrent.TimeUnit;
 
 public class PDBAtomRetriever {
 
-	/*
-	 * Class that returns all 'ATOM' entries (type String) from a .cif/.pdb file
-	 * into a StringBuffer (getAtoms2) or String (getAtoms) object
+	/**
+	 * From a StringBuilder that regroups all the content of a cif file,
+	 * PDBAtomRetriver search all ATOM entries and put them into Atom object form
 	 */
 
 	private String pdbFileContent;
@@ -43,6 +44,7 @@ public class PDBAtomRetriever {
 
 	}
 
+	//EXECUTION TIME > getAtoms... delete ?
 	public String getAtoms2() throws IOException {
 
 		Reader reader = new FileReader(pdbFileContent);
@@ -58,6 +60,48 @@ public class PDBAtomRetriever {
 		bufferedReader.close();
 		return startWithATOM;
 
+	}
+
+	// FOR TESTING PURPOSES :
+	public static void main(String[] args) {
+
+		String filenameDebian = "/home/karine/src/java/ProjetPDB/doc/3bw7.cif";
+		String filenameMacOS = "/Users/dias/eclipse-workspace/ProjetPDB/doc/2b5i.cif";
+		String filenameWindows = "C:\\Users\\Karine\\eclipse-workspace\\ProjetPDB\\doc\\2b5i.cif";
+
+		PDBAtomRetriever testFile = new PDBAtomRetriever(filenameDebian);
+
+		
+		try {
+			// measure execution time of first method getAtoms() : 
+			long startTime = System.nanoTime();
+			StringBuffer listOfAtoms1 = testFile.getAtoms();
+			long endTime = System.nanoTime();
+			long durationInSeconds = TimeUnit.NANOSECONDS.toSeconds(endTime - startTime);
+			
+			
+			// measure execution time of second method getAtoms() : 
+			long startTime2 = System.nanoTime();
+			String listOfAtoms2 = testFile.getAtoms2();
+			long endTime2 = System.nanoTime();
+			long durationInSeconds2 = TimeUnit.NANOSECONDS.toSeconds(endTime2 - startTime2);
+
+
+			
+			
+			//print all atoms : 
+			System.out.println(listOfAtoms1.toString());
+			System.out.println("\n\n");
+			System.out.println(listOfAtoms2);
+			System.out.println("Execution time of method getAtoms = " + durationInSeconds);
+			System.out.println("Execution time of method getAtoms2 = " + durationInSeconds2);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
