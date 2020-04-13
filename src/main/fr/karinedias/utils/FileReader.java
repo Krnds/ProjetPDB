03e59061a45750173ac0d69221348b48936c34e6
Karine Dias <harlequinuniform@gmail.com>
2020-04-13 22:15:03 +0200

    Add method for searching for Atom lines in FileReader class

--- src/main/fr/karinedias/utils/FileReader.java
+++ src/main/fr/karinedias/utils/FileReader.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 //for the measure of elapsed time :
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
@@ -21,41 +24,62 @@ public class FileReader {
 	 */
 
 	private final FetchStructure structureFile;
-	// file paths to delete...
-	private final String filePathMacOS = "/Users/dias/eclipse-workspace/ProjetPDB/doc/6hk2.cif";
-	private final String fileWindows = "C:\\Users\\Karine\\eclipse-workspace\\ProjetPDB\\doc\\2b5i.cif";
+	private String filePath;
+
 
 	public FileReader() {
-		// initialize FetchStructure object here ?
+		//TODO: change bad pratice here
 		this.structureFile = new FetchStructure();
 		structureFile.getPDBFile();
+		this.filePath = structureFile.getPath();
 	}
 
-	public String getFileName() {
 
-		if (System.getProperty("os.name").equalsIgnoreCase("Mac OS X")) {
-			return filePathMacOS;
-		} else if (System.getProperty("os.name").equalsIgnoreCase("Windows 7")) {
-			return fileWindows;
-		} else {
-			return structureFile.getPathStructure();
-		}
+//TODO: create a method for all OS compatible paths
+
+	protected String getFilePath() {
+		return filePath;
 	}
+
+
 
 	/*
 	 * Test of reader : prints all the file content
 	 */
 	// source : https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-	public StringBuilder reader(String filePath) {
+	public StringBuilder reader(String path) {
 		StringBuilder contentBuilder = new StringBuilder();
-		try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
+		try (Stream<String> stream = Files.lines(Paths.get(path), StandardCharsets.UTF_8)) {
 			stream.forEach(s -> contentBuilder.append(s).append("\n"));
 		} catch (FileNotFoundException exception) {
-			System.out.println("The file " + filePath + " was not found.");
+			System.out.println("The file " + path + " was not found.");
 		} catch (IOException exception) {
 			System.out.println(exception);
 		}
 		return contentBuilder;
+	}
+	
+
+	//TODO: 13/04/2020
+	public Set<String> getAtomEntries(StringBuilder content) {
+		
+		//TODO initial capacity and load factor of hashset ?
+		HashSet<String> atomEntries = new HashSet<String>(); //spécifier init capacity & load factor
+		
+		String delimiter = "\n";
+		
+	String contentToString = content.toString();
+	
+	String[] array = new String[contentToString.length()];
+	Set<String> set = new HashSet<>(Arrays.asList(array));
+	//set.addAll(contentToString.split("\n"));
+	
+
+ 
+    
+		
+		return set;
+		
 	}
 
 	// TO DELETE
@@ -64,8 +88,11 @@ public class FileReader {
 		long startTime = System.nanoTime();
 
 		FileReader test = new FileReader();
-		System.out.println("Testing the FileReader reader method....\n");
-		System.out.println(test.reader(test.getFileName()));
+		System.out.print("The file downloaded can be found at : ");
+		System.out.println(test.getFilePath());
+		
+		System.out.println("Testing the FileReader reader method...\n");
+		System.out.println(test.reader(test.getFilePath()));
 
 		long endTime = System.nanoTime();
 
