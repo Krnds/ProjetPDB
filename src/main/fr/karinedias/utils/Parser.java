e7e5e72feff8d339e2effb4c12cb24f29c0be921
Karine Dias <harlequinuniform@gmail.com>
2020-05-17 23:20:43 +0200

    Add inner parser classes for each object into Parser

--- src/main/fr/karinedias/utils/Parser.java
+++ src/main/fr/karinedias/utils/Parser.java
@@ -2,8 +2,12 @@ package src.main.fr.karinedias.utils;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import src.main.fr.karinedias.model.Molecule;
 
 public class Parser {
 
@@ -13,11 +17,24 @@ public class Parser {
 
 	@SuppressWarnings("unused")
 	private StringBuilder contentOfFile = null;
+	public Molecule Molecule;
+	
+	//general method for extracting lines of interest in cif file
+	private String getBetweenStrings(String text, String textFrom, String textTo) {
+
+		String result = "";
+		// Cut the beginning of the text to not occasionally meet a
+		// 'textTo' value in it:
+		result = text.substring(text.indexOf(textFrom) + textFrom.length(), text.length());
+		// Cut the excessive ending of the text:
+		result = result.substring(0, result.indexOf(textTo));
+		return result;
+	}
 
 	/**
 	 * Use inner classes for parsing each object needed
 	 * 
-	 * @author karine
+	 * @author Karine Dias
 	 *
 	 */
 	public class MoleculeParser {
@@ -27,14 +44,49 @@ public class Parser {
 			Parser.this.contentOfFile = contentOfFile;
 		}
 
+		private String parseEntryID() {
+
+			String id = null;
+			for (String string : contentOfFile.toString().split("\\n")) {
+				if (string.startsWith("_entry.id")) {
+					Pattern idPattern = Pattern.compile("([A-Z]|[1-9]){4}");
+					Matcher m = idPattern.matcher(string);
+					if (m.find())
+						id = m.group();
+				}
+
+			}
+			return id;
+		}
+
+		private List<Character> parseChainsOfMolecule() {
+			
+			String lines = contentOfFile.toString();
+			String[] linesToSearch = getBetweenStrings(lines, "_struct.entry_id",
+					"_atom_site.group_PDB").split("\\n");
+			List<String> listOfLinesToSearch = Arrays.asList(linesToSearch);
+			// Filter chains of molecule by their name (one capital letter)
+			List<Character> chains = listOfLinesToSearch.stream()
+					.filter(Pattern.compile("(^[A-Z])\\sN").asPredicate())
+					.map(s -> s.charAt(0))
+					.collect(Collectors.toList());
+			if (chains.isEmpty())
+				//TODO: cases of empty chains ?
+				//search for chains in atom parser or initialize to null ?
+				chains.add('\u0000');//add null character
+			return chains;
+			
+		}
+
 		private void parseMoleculeLines() {
 
 			String lines = contentOfFile.toString();
 			String[] linesToSearch = getBetweenStrings(lines, "_entity.pdbx_number_of_molecules",
 					"_entity_poly.entity_id").split("\\n");
 			List<String> listOfLinesToSearch = Arrays.asList(linesToSearch);
-			// Chains entities are of three types: polymer, non-polymer and water
-			List<String> molecules = listOfLinesToSearch.stream().filter(Pattern.compile("^\\d\\s(polymer|non-polymer|water)").asPredicate())
+			// Filter molecule lines with chains entities : polymer, non-polymer and water
+			List<String> molecules = listOfLinesToSearch.stream()
+					.filter(Pattern.compile("^\\d\\s(polymer|non-polymer|water)").asPredicate())
 					.collect(Collectors.toList());
 
 			for (int i = 0; i < molecules.size(); i++) {
@@ -43,30 +95,55 @@ public class Parser {
 
 		}
 
-		private String getBetweenStrings(String text, String textFrom, String textTo) {
 
-			String result = "";
-
-			// Cut the beginning of the text to not occasionally meet a
-			// 'textTo' value in it:
-			result = text.substring(text.indexOf(textFrom) + textFrom.length(), text.length());
-
-			// Cut the excessive ending of the text:
-			result = result.substring(0, result.indexOf(textTo));
-
-			return result;
-		}
 
 	}
 
 	// TESTING :
+	//TODO create method for returning Molecule object + Chain object ? + Residue and Atom objects
 
 	public static void main(String[] args) {
-		String fileTestPath = "/home/karine/src/java/ProjetPDB/doc/2b5i.cif";
-		FileReader filereader = new FileReader(fileTestPath);
+		long startTime = System.nanoTime();
+		String file1 = "/home/karine/src/java/ProjetPDB/doc/4NCC.cif";
+		String file2 = "/home/karine/src/java/ProjetPDB/doc/3dcu.cif";
+		String file3 = "/home/karine/src/java/ProjetPDB/doc/3l3t.cif";
+		
+		FileReader filereader = new FileReader(file3);
 		StringBuilder content = filereader.reader();
 		Parser p = new Parser();
 		MoleculeParser test = p.new MoleculeParser(content);
 		test.parseMoleculeLines();
+
+		long endTime = System.nanoTime();
+		long durationInNano = (endTime - startTime); // Total execution time in nano seconds
+		long durationInMilliseconds = TimeUnit.NANOSECONDS.toMillis(durationInNano);
+		System.out.println("Elapsed time in milliseconds = " + durationInMilliseconds);
+		System.out.println("ID: " + test.parseEntryID());
+		test.parseChainsOfMolecule();
+	}
+	
+	public class ChainParser {
+		
+		public ChainParser(StringBuilder contentOfFile) {
+
+			Parser.this.contentOfFile = contentOfFile;
+		}
+	}
+	
+	public class ResidueParser {
+		
+		public ResidueParser(StringBuilder contentOfFile) {
+
+			Parser.this.contentOfFile = contentOfFile;
+		}
+	}
+	
+	
+	public class AtomParser {
+		
+		public AtomParser(StringBuilder contentOfFile) {
+
+			Parser.this.contentOfFile = contentOfFile;
+		}
 	}
 }
