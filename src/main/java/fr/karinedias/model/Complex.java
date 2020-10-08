6a79a953142ea01f8322c8ba4fa34957695f9250
Karine <harlequinuniform@gmail.com>
2020-10-08 15:24:22 +0200

    Fix setters and getters

--- src/main/java/fr/karinedias/model/Complex.java
+++ src/main/java/fr/karinedias/model/Complex.java
@@ -12,20 +12,39 @@ public class Complex {
 		this.molecules = molecules;
 	}
 
-	protected List<Molecule> getMolecules() {
-		return molecules;
-	}
-
-	protected void setMolecules(List<Molecule> molecules) {
-		this.molecules = molecules;
-	}
-
-	protected String getEntryID() {
+	/**
+	 * @return the entryID
+	 */
+	public String getEntryID() {
 		return entryID;
 	}
 
-	public static void main(String[] args) {
-
+	/**
+	 * @param entryID the entryID to set
+	 */
+	public void setEntryID(String entryID) {
+		this.entryID = entryID;
 	}
+
+	/**
+	 * @return the molecules
+	 */
+	public List<Molecule> getMolecules() {
+		return molecules;
+	}
+
+	/**
+	 * @param molecules the molecules to set
+	 */
+	public void setMolecules(List<Molecule> molecules) {
+		this.molecules = molecules;
+	}
+
+	@Override
+	public String toString() {
+		return "Complex [entryID=" + entryID + ", molecules=" + molecules + "]";
+	}
+
+
 
 }
