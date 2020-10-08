d0826d53f90357b27456f1d3b874d56c4c77b5e2
Karine <harlequinuniform@gmail.com>
2020-10-08 15:22:46 +0200

    Clean up NeighborSearch and add method to display minimal distance interaction between residues

--- src/main/java/fr/karinedias/math/NeighborSearch.java
+++ src/main/java/fr/karinedias/math/NeighborSearch.java
@@ -11,32 +11,66 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import fr.karinedias.model.Chain;
+import fr.karinedias.model.Complex;
 import fr.karinedias.model.Molecule;
 import fr.karinedias.model.Residue;
 
 public class NeighborSearch {
 
-	List<Molecule> molecules;
-
-	// get all residues within a distance X from a residue
+	// get all residues within a distance D from a residue
 	/**
 	 * 
-	 * @param molecule : Polymer containing all the residues to search for
-	 * @param baseRes  : TODO: a residue from the molecule or from another ??
-	 * @param distance : minimal distance between the residue and
-	 * @return a list of nearest neighbors residues within the parameter residue
+	 * @param complex : complex made of multiple molecules with multiples residues
+	 * @param baseRes  : random residue
+	 * @param distance  : cutoff distance in Angströms
+	 * @return a list of nearest neighbors
 	 */
-	public static List<Residue> getNeighborsResidues(Molecule molecule, Residue baseRes, int distance) {
+	public static List<Residue> getNeighborsResidues(Complex complex, Residue baseRes, int distance) {
 
-		List<Residue> residues = molecule.getResidues();
+		List<Residue> residuesOfComplex = new ArrayList<>();
+
+		for (Molecule mol : complex.getMolecules()) {
+			residuesOfComplex.addAll(mol.getResidues());
+		}
 		List<Residue> neighbors = new ArrayList<>();
-		for (int i = 0; i < residues.size(); i++) {
-			if (ResidueDistanceCalculation.distanceBetweenResidues(baseRes, residues.get(i)) < distance) {
-				neighbors.add(residues.get(i));
+		for (int i = 0; i < residuesOfComplex.size(); i++) {
+			if (ResidueDistanceCalculation.distanceBetweenResidues(baseRes, residuesOfComplex.get(i)) < distance) {
+				neighbors.add(residuesOfComplex.get(i));
 			}
 		}
 
 		return neighbors;
+	}
+	
+	// get all distances from a residue
+	/**
+	 * 
+	 * @param complex : complex made of multiple molecules with multiples residues
+	 * @param baseRes  : random residue
+	 * @param distance  : cutoff distance in Angströms
+	 * @return a list of the smallest distances between the residue and his neighbors
+	 */
+	public static List<Double> getNeighborsDistance(Complex complex, Residue baseRes, int distance) {
+
+		List<Residue> residuesOfComplex = new ArrayList<>();
+
+		for (Molecule mol : complex.getMolecules()) {
+			residuesOfComplex.addAll(mol.getResidues());
+		}
+		List<Double> neighborsDistances = new ArrayList<>();
+		for (int i = 0; i < residuesOfComplex.size(); i++) {
+			// if baseRes is compared with himself, skip distance calculation
+			if (residuesOfComplex.get(i).equals(baseRes))
+					i++;
+			double d = ResidueDistanceCalculation.distanceBetweenResidues(baseRes, residuesOfComplex.get(i));
+			if (d < distance) {
+				neighborsDistances.add(d);
+			}
+		}
+		//sort distances
+		neighborsDistances.sort(Comparator.naturalOrder());
+
+		return neighborsDistances;
 	}
 
 	/**
@@ -53,7 +87,7 @@ public class NeighborSearch {
 		List<Residue> residuesMol1 = firstMol.getResidues();
 		List<Residue> residuesMol2 = secondMol.getResidues();
 		List<Residue> neighbors = new ArrayList<>();
-		
+
 		for (int i = 0; i < residuesMol1.size(); i++) {
 			for (int j = 0; j < residuesMol2.size(); j++) {
 				if (ResidueDistanceCalculation.distanceBetweenResidues(residuesMol1.get(i),
@@ -81,25 +115,27 @@ public class NeighborSearch {
 
 	}
 
-	public static List<Residue> getNeighborsFromChains(Molecule cytokine, Molecule receptor, int cutoff, boolean sorted) {
-		
+	public static List<Residue> getNeighborsFromChains(Molecule cytokine, Molecule receptor, int cutoff,
+			boolean sorted) {
+
 		// get chains of cytokine
 		List<Character> chainsOfCytokine = new ArrayList<Character>();
 		HashMap<Character, List<Residue>> residuesCytokineChains = new HashMap<>();
 		List<Residue> neighbors = new ArrayList<>();
-		
-		
-		// For each chain of Cytokine, filter residues from this chain and search all possible neighbors from the receptor molecule
+
+		// For each chain of Cytokine, filter residues from this chain and search all
+		// possible neighbors from the receptor molecule
 		for (Chain chainChar : cytokine.getChain()) {
 			System.out.println(chainChar.getChain());
-			residuesCytokineChains.put(chainChar.getChain(),cytokine.getResidues().stream().filter(r -> r.getAltChain() == chainChar.getChain()).collect(Collectors.toList()));
-			
-			//convert all values of HashMap to list 
+			residuesCytokineChains.put(chainChar.getChain(), cytokine.getResidues().stream()
+					.filter(r -> r.getAltChain() == chainChar.getChain()).collect(Collectors.toList()));
+
+			// convert all values of HashMap to list
 			List<Residue> cytokineResidues = new ArrayList<Residue>();
 			cytokineResidues.addAll(cytokine.getResidues());
-			
+
 			// search neighbors
-			for (int i = 0; i < cytokineResidues.size(); i++) { //TODO: old = residuesCytokineChains.keySet().size()
+			for (int i = 0; i < cytokineResidues.size(); i++) { // TODO: old = residuesCytokineChains.keySet().size()
 				for (int j = 0; j < receptor.getResidues().size(); j++) {
 					if (ResidueDistanceCalculation.distanceBetweenResidues(cytokineResidues.get(i),
 							receptor.getResidues().get(j)) <= cutoff) {
@@ -108,22 +144,24 @@ public class NeighborSearch {
 				}
 			}
 		}
-		
+
 		// get chains of receptor
 		List<Character> chainsOfReceptor = new ArrayList<Character>();
 		HashMap<Character, List<Residue>> residuesReceptorChains = new HashMap<>();
-		
-		// For each chain of Receptor, filter residues from this chain and search all possible neighbors from the cytokine molecule
+
+		// For each chain of Receptor, filter residues from this chain and search all
+		// possible neighbors from the cytokine molecule
 		for (Chain chainChar : receptor.getChain()) {
 			System.out.println(chainChar.getChain());
-			residuesReceptorChains.put(chainChar.getChain(),receptor.getResidues().stream().filter(r -> r.getAltChain() == chainChar.getChain()).collect(Collectors.toList()));
-			
-			//convert all values of HashMap to list 
+			residuesReceptorChains.put(chainChar.getChain(), receptor.getResidues().stream()
+					.filter(r -> r.getAltChain() == chainChar.getChain()).collect(Collectors.toList()));
+
+			// convert all values of HashMap to list
 			List<Residue> receptorResidues = new ArrayList<Residue>();
 			receptorResidues.addAll(receptor.getResidues());
-			
+
 			// search neighbors
-			for (int i = 0; i < receptorResidues.size(); i++) { //TODO: old = receptorCytokineChains.keySet().size()
+			for (int i = 0; i < receptorResidues.size(); i++) { // TODO: old = receptorCytokineChains.keySet().size()
 				for (int j = 0; j < cytokine.getResidues().size(); j++) {
 					if (ResidueDistanceCalculation.distanceBetweenResidues(receptorResidues.get(i),
 							cytokine.getResidues().get(j)) <= cutoff) {
@@ -132,7 +170,6 @@ public class NeighborSearch {
 				}
 			}
 		}
-		
 
 		// remove duplicates from neighbors list
 		List<Residue> neighborsWithoutDuplicates = neighbors.stream().distinct().collect(Collectors.toList());
